f0e180ba6da5161d5f8ae4df7647a730f0a7ffa5
heejoo <heejoo_byun@naver.com>
2023-04-28 15:56:42 +0900

    3차 세미나 메인 페이지 시작

---
+++ GO_SOPT_Seminar_Assignment/ThirdSeminar/MainPage/MainPageTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainPageTableViewCell.swift
+//  GO_SOPT_Seminar_Assignment
+//
+//  Created by 변희주 on 2023/04/28.
+//
+
+import UIKit
+
+final class MainPageTableViewCell: UITableViewCell {
+
+
+}
---
+++ GO_SOPT_Seminar_Assignment/ThirdSeminar/MainPage/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  GO_SOPT_Seminar_Assignment
+//
+//  Created by 변희주 on 2023/04/24.
+//
+
+import UIKit
+
+final class MainViewController: BaseViewController {
+        
+
+}
